6d1e7c6a64cab08f2fa8a66cc7244ab286d4dc41
Vladimir Borisov <vladimir.borisov@x3me.net>
2018-02-27 14:49:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nautilus image dimensions
+
+Nautilus script to show image dimensions in list view column
+
+## Instalation
+
+Copy the script to `~/.local/share/nautilus-python/extensions/` or `/usr/share/nautilus-python/extensions/`.
+
+Restart Nautilus
---
+++ image-dimensions.py
@@ -0,0 +1,30 @@
+import urllib
+import gi
+gi.require_version('Nautilus', '3.0')
+from gi.repository import Nautilus, GObject, Gtk
+from PIL import Image
+
+class ColumnExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoProvider):
+    def __init__(self):
+        pass
+
+    def get_columns(self):
+        return Nautilus.Column(name="NautilusPython::image_dimensions_column",
+                               attribute="image_dimensions",
+                               label="Image dimensions",
+                               description="Get image dimensions"),
+
+    def update_file_info(self, file):
+        if file.get_uri_scheme() != 'file' or file.get_mime_type()[0:5] != 'image':
+            file.add_string_attribute('image_dimensions', '')
+            return
+        
+        filename = urllib.unquote(file.get_uri()[7:])
+        size = ''
+        try:
+            im = Image.open(filename)
+            size = 'x'.join(map(str, list(im.size)))
+        except IOError:
+            pass
+        file.add_string_attribute('image_dimensions', size)
+
